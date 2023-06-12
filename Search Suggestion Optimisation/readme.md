86ca3c8767be2930b92ff9dd91f5224483a12884
Saurabh Ghiya <saurabhghiya@gmail.com>
2023-06-13 03:35:37 +0530

    Search Component Functionality completed

--- Search Suggestion Optimisation/readme.md
+++ Search Suggestion Optimisation/readme.md
@@ -1,1 +1,5 @@
+#Search Component
+
 Optimizing search suggestion network calls using debounce in JS
+Search API used => 'https://demo.dataverse.org/api/search?q='
+Search API Guide => https://guides.dataverse.org/en/latest/api/search.html
--- Search Suggestion Optimisation/script.js
+++ Search Suggestion Optimisation/script.js
@@ -1,30 +1,51 @@
-// let urlString = 'https://demo.dataverse.org/api/search?q=value';
-// let data;
 let searchbar = document.querySelector('.searchbar');
-let delay = 1000;
-let searchHandler = debounce(fn, delay);
 let c = 1;
-/* 
+let searchHandler = debounce(dataHandler, 500);
+const urlBase = 'https://demo.dataverse.org/api/search?q=';
+
+let resultContainer = document.querySelector('.results');
+
 async function getData(url){
     let response = await fetch(url);
-    let testdata = await response.json();
-    data = testdata.data;
-    console.log(data);
+    let newdata = await response.json();
+    let data = [ ...newdata.data.items ];
+    if(data.length == 0) {
+        resultContainer.style.display = 'none';
+        return;
+    }
+
+    for(let i=0; i<data.length; i++){
+        data[i] = data[i].name;
+    }
+    showItems(data);
 }
 
-getData(urlString);
- */
-function fn(){
+function dataHandler(){
     console.log('Network Call',c++);
+    let keyword = searchbar.value;
+    let urlString = urlBase + keyword;
+    
+    getData(urlString);
+}
+
+function showItems(data){
+    resultContainer.innerHTML = '';
+    for(let i of data){
+        let item = document.createElement('p');
+        item.className = 'item';
+        item.innerText = i;
+        resultContainer.appendChild(item);
+    }
+    resultContainer.style.display = 'block';
 }
 
 function debounce(cb, time){
     let timeout;
     
-    // if(searchbar.value == '') return clearTimeout(timeout);
     return function (){
         clearTimeout(timeout);
         if(searchbar.value != '') timeout = setTimeout(cb, time);
+        else resultContainer.style.display = 'none';
     }
 }
 
